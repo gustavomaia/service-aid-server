c775a1f3f6c404856cb8b503fd8cb91f4e94ba35
Gustavo Maia <gustavomaia@outlook.com>
2017-05-01 13:46:09 -0300

    Adiciona endpoint para buscar ordens de serviço do emissor

--- controllers/serviceOrderController.js
+++ controllers/serviceOrderController.js
@@ -1,16 +1,41 @@
 var db = require('../configuration/database');
 
 module.exports = function(app) {
-  var serviceOrderController = {
+  let serviceOrderController = {
     create: function(req, res) {
       let serviceOrderRequest = req.body;
 
       db.ServiceOrder
-        .create({description: serviceOrderRequest.description, place: serviceOrderRequest.place, status: 'created'}).then(function (newOS) {
+        .create({description: serviceOrderRequest.description, place: serviceOrderRequest.place, status: 'waiting_management'}).then(function (newOS) {
             newOS.setIssuer(req.user);
         });
 
       res.sendStatus(201);
+    },
+  issuer: function(req, res) {
+      let loggedUser = req.user;
+      db.ServiceOrder.findAll({
+        where: {
+          userIssuerId: loggedUser.id,
+          status: 'managed'
+        }
+      }).then(function (foundManagedServiceOrders) {
+        db.ServiceOrder.findAll({
+          where: {
+            userIssuerId: loggedUser.id,
+            status: 'waiting_management'
+          }
+        })
+        .then(function (foundWaitingManagementServiceOrders) {
+          let response = {
+            managed: foundManagedServiceOrders,
+            waitingManagement: foundWaitingManagementServiceOrders
+          }
+
+          res.status(200).json(response)
+          }
+        )
+      });
     }
   }
 
--- routes/serviceOrderRoutes.js
+++ routes/serviceOrderRoutes.js
@@ -2,4 +2,5 @@ module.exports = function(app){
   var serviceOrderController = app.controllers.serviceOrderController;
 
   app.post('/service-order', serviceOrderController.create);
+  app.get('/service-order/issuer', serviceOrderController.issuer);
 }
